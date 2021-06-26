602f14f838d12955cbf7865dc8ab124253f2c30a
samirehsani19 <samirehsani19@gmail.com>
2021-06-26 19:39:06 +0200

    JQwigets grid

---
+++ JQWidgets/Controllers/CustomerController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks;
+using JQWidgets.Models;
+using System.Text.Json;
+
+namespace JQWidgets.Controllers
+{
+    public class CustomerController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public List<Customer> GetCustomers()
+        {
+            return new List<Customer>
+            {
+                new Customer{Id = 1, Name = "Samir", Age = 25, Gender = Gender.Male},
+                new Customer{Id = 2, Name = "Markus", Age = 20, Gender = Gender.Male},
+                new Customer{Id = 3, Name = "Hamid", Age = 25, Gender = Gender.Male},
+                new Customer{Id = 4, Name = "Samira", Age = 25, Gender = Gender.Female},
+                new Customer{Id = 5, Name = "Hanna", Age = 35, Gender = Gender.Female},
+                new Customer{Id = 6, Name = "Isaq", Age = 23, Gender = Gender.Male},
+                new Customer{Id = 7, Name = "Anders", Age = 45, Gender = Gender.Male},
+                new Customer{Id = 8, Name = "Imma", Age = 25, Gender = Gender.Female},
+                new Customer{Id = 9, Name = "Robert", Age = 18, Gender = Gender.Male},
+                new Customer{Id = 10, Name = "Gustav", Age = 27, Gender = Gender.Male},
+                new Customer{Id = 11, Name = "Jolly", Age = 23, Gender = Gender.Female},
+                new Customer{Id = 12, Name = "Jahn", Age = 36, Gender = Gender.Male},
+                new Customer{Id = 13, Name = "Pernilla", Age = 34, Gender = Gender.Female},
+                new Customer{Id = 14, Name = "Per", Age = 37, Gender = Gender.Male},
+                new Customer{Id = 15, Name = "Gunilla", Age = 47, Gender = Gender.Female},
+            };
+        }
+
+    }
+}
---
+++ JQWidgets/Models/Customer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace JQWidgets.Models
+{
+    public class Customer
+    {
+        public int Id { get; set; }     
+        public string Name { get; set; }     
+        public int Age { get; set; }     
+        public Gender Gender { get; set; }     
+    }
+
+    [JsonConverter(typeof(JsonStringEnumConverter))]
+    public enum Gender
+    {
+        Male,
+        Female
+    }
+}
